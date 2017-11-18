50c49f2f6fadc243c2082082e9abe74f3679a38f
Bo0km4n <pokopenkatuya@gmail.com>
2017-11-18 11:27:07 +0900

    rm export

--- zsh.d/export.zsh
+++ zsh.d/export.zsh
@@ -13,5 +13,4 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 export PATH="/Library/TeX/texbin:$PATH"
 eval "$(pyenv init -)"
 export PATH="/Users/bo0km4n/.local/lib/python3.6/site-packages:$PATH"
-export ECR_URI_BASE=117869718761.dkr.ecr.ap-northeast-1.amazonaws.com
 export PATH="/Users/bo0km4n/go/src/github.com/go/bin:$PATH"
